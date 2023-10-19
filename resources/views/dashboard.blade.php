d72b32db4aa108c05111e583ec18104bc63d2bd9
Pedro de Souza <pedrohodesouza@gmail.com>
2023-10-19 16:21:23 -0300

    Implementa a API com INSERT

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -15,6 +15,42 @@
                         <div class="w-full sm:max-w-md px-8 py-4 shadow-md overflow-hidden sm:rounded-lg rounded-lg" style="background: rgb(34,149,150);  background: linear-gradient(266deg, rgba(34,149,150,1) 0%, rgba(131,218,160,1) 86%);">
                             <div class="mt-6 space-y-1">
 
+<<<<<<< HEAD
+                                    <?php
+
+                                        $hostname = "localhost";
+                                        $username = "root";
+                                        $password = "masterkey";
+                                        $database = "carteira";
+
+                                        $mysqli = new mysqli($hostname, $username, $password, $database);
+
+                                        if ($mysqli->connect_error) {
+                                            die("Erro ao trazer os ativos: " . $mysqli->connect_error);
+                                        }else {
+
+                                            $sql = "SELECT nomeAtivo, valorAtivo FROM ativos_financeiros ORDER BY nomeAtivo";
+                                            $result = $mysqli->query($sql);
+                                        
+                                            if ($result->num_rows > 0) {
+                                                echo '<ul>';
+                                                while ($row = $result->fetch_assoc()) {
+                                                    echo '<li>' . $row['nomeAtivo'] . ', Valor: ' . $row['valorAtivo'] . 'R$</li>';
+                                                }
+                                                echo '</ul>';
+                                            } else {
+                                                echo 'Nenhum ativo cadastrado.';
+                                            }
+
+
+                                          
+                                        }
+                                        
+
+                                    ?>
+
+=======
+>>>>>>> 243ff5241420bbf0bedc9382b2a2f4397c9e2caf
                                     <!--Aqui vai ser inserido todo e qualquer código referente ao dashboard 
                                         O dashboard será nomeado de carteira e irá conter:
                                         - Todo os retornos da API
