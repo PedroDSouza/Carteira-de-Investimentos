ab5d53eccad91fc4349d9ef572a2e1f6be535d37
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2023-11-19 16:59:35 -0300

    updates
    updates

--- resources/views/profile/partials/image-profile.blade.php
+++ resources/views/profile/partials/image-profile.blade.php
@@ -1,8 +1,6 @@
 <section>
 
-
-    <div class="hidden lg:block w-full sm:max-w-md  overflow-hidden sm:rounded-lg">
-        <img class="object-cover h-full bg-cover rounded-l-lg" src="./images/imgPerfil.svg" alt="Carteira">
+    <div class="hidden lg:block w-full sm:max-w-md overflow-hidden sm:rounded-lg flex items-center justify-center">
+        <img class="object-cover w-auto h-full rounded-l-lg" src="./images/imgPerfil.svg" alt="Carteira">
     </div>
-
 </section>
