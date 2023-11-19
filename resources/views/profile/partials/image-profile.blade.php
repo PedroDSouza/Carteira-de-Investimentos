44a31471cbbf08a14cb61fa221461855be3c35cb
Lucas Batista <65094987+eulucasbatista@users.noreply.github.com>
2023-11-19 16:53:10 -0300

    New updates
    New updates

---
+++ resources/views/profile/partials/image-profile.blade.php
@@ -0,0 +1,8 @@
+<section>
+
+
+    <div class="hidden lg:block w-full sm:max-w-md  overflow-hidden sm:rounded-lg">
+        <img class="object-cover h-full bg-cover rounded-l-lg" src="./images/imgPerfil.svg" alt="Carteira">
+    </div>
+
+</section>
